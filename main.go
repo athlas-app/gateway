0db3168234a3f48983c9eebbaa6ad8c0a44139c4
Gabriel Timotei <gabtimotei@gmail.com>
2023-03-26 16:16:31 -0400

    Modified to use correct TLS cert for Cloud Run

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"log"
 	"net/http"
 	"os"
@@ -10,7 +11,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 
 	playPb "github.com/athlas-app/play/pb"
 )
@@ -24,19 +25,26 @@ func main() {
 	mux := runtime.NewServeMux()
 
 	playServerUrl := os.Getenv("PLAY_SERVER_URL") // Replace with `localhost:8080` if you want to run the gRPC server locally
-	playPbErr := playPb.RegisterPlayHandlerFromEndpoint(context.Background(), mux, playServerUrl, []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())})
+
+	// Create TLS credentials for gRPC client
+	creds := credentials.NewTLS(&tls.Config{
+		// Use InsecureSkipVerify if you don't have a valid CA certificate for your Cloud Run service.
+		// It's not recommended for production use.
+		InsecureSkipVerify: true,
+	})
+
+	println(playServerUrl)
+	playPbErr := playPb.RegisterPlayHandlerFromEndpoint(context.Background(), mux, playServerUrl, []grpc.DialOption{grpc.WithTransportCredentials(creds)})
 	if playPbErr != nil {
 		log.Fatalf("failed to register gateway: %v", playPbErr)
 	}
 
-	r.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
-		mux.ServeHTTP(w, r)
-	})
+	r.Mount("/", mux)
 
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	log.Default().Println("Starting gRPC gateway on port " + port)
-	http.ListenAndServe(":" + port, r)
+	http.ListenAndServe(":"+port, r)
 }
